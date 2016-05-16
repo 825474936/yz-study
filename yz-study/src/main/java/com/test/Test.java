b966b6ee5c22e8ee2e9dcead0960a552376c03b1
yzs <825474936@qq.com>
2016-05-16 21:35:44 +0800

    fix

--- yz-study/src/main/java/com/test/Test.java
+++ yz-study/src/main/java/com/test/Test.java
@@ -1,8 +1,11 @@
 package com.test;
 
+import java.util.ArrayList;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("11");
+		ArrayList<String> list = new ArrayList<String>();
 	}
 
 }
